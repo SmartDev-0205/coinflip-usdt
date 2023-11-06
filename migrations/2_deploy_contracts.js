5153d5b2d2f26506e7aa6a5bea86e927c97b80cf
smartdev-0205 <smartdevpro001@gmail.com>
2023-11-06 20:57:35 +0900

    remove homepage in package.json

--- migrations/2_deploy_contracts.js
+++
@@ -1,5 +0,0 @@
-const Coinflip = artifacts.require("Coinflip");
-
-module.exports = function (deployer, network, accounts) {
-  deployer.deploy(Coinflip,{value: web3.utils.toWei(".01", "ether"), from: accounts[0]});
-};
